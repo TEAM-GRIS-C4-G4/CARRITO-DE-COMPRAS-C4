3a15b2f16dc5deadf8f4fffcd21c733a92c880c4
Cfcubillos <113119481+Cfcubillos@users.noreply.github.com>
2022-12-19 13:03:54 -0500

    Carpeta estructura SPA para el Frontend

---
+++ frontend/mpa/js/listData.js
@@ -0,0 +1,47 @@
+function findAllDepartamentos() {
+    $.ajax({
+        url: 'http://132.145.204.101:8080/api/departments',
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (items) {
+        var listData = "<option disabled selected='true' >-- Seleccione --</option>";
+        items.forEach(function (item, index, array) {
+            listData += `<option value='`+item.id+`'>`+item.nombre+`</option>`;
+        })
+        $("#departmentId").html(listData);
+    })
+}
+
+function findAllCiudades() {
+    $.ajax({
+        url: 'http://132.145.204.101:8080/api/cities',
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (items) {
+        var listData = "<option disabled selected='true' >-- Seleccione --</option>";
+        items.forEach(function (item, index, array) {
+            listData += `<option value='`+item.id+`'>`+item.nombre+`</option>`;
+        })
+        $("#ciudadId").html(listData);
+    })
+}
+
+function findAllUsuarios() {
+    $.ajax({
+        url: 'http://132.145.204.101:8080/api/users',
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (items) {
+        var listData = "<option disabled selected='true' >-- Seleccione --</option>";
+        items.forEach(function (item, index, array) {
+            listData += `<option value='`+item.id+`'>`+item.usuario+`</option>`;
+        })
+        $("#usuarioId").html(listData);
+    })
+}
